c7e4944a5021234dba23a216aecd8161adfdf60b
Josh <josh@magicmoments.com>
2013-10-31 16:28:48 -0400

    added ability to check for unique status of something

---
+++ Extensions/CloudTableClientExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.WindowsAzure.StorageClient;
+
+using JoshCodes.Persistence.Azure.Storage.Extensions;
+
+namespace JoshCodes.Persistence.Azure.Storage
+{
+    internal class UniquenessEntity : TableServiceEntity
+    {
+
+    }
+
+    public static class CloudTableClientExtensions
+    {
+        public static bool TryRegisterUnique(this CloudTableClient tableClient, string uniqueId, string ns)
+        {
+            var entityTableName = typeof(UniquenessEntity).Name;
+            tableClient.CreateTableIfNotExist(entityTableName);
+
+            var entity = new UniquenessEntity()
+            {
+                RowKey = uniqueId,
+                PartitionKey = ns,
+            };
+
+            try
+            {
+                var tableServiceContext = tableClient.GetDataServiceContext();
+                tableServiceContext.AddObject(entityTableName, entity);
+                tableServiceContext.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (ex.IsProblemResourceAlreadyExists())
+                {
+                    return false;
+                }
+                throw;
+            }
+        }
+    }
+}
